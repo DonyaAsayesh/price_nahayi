5738cd73c2b377e8baeb4411a9a032aa0986002c
Donya <donya.asayesh@outlook.com>
2023-09-21 15:38:43 +0330

    initial commit

---
+++ lib/class/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('fa'), // farsi
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:price_nahayi/class/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
