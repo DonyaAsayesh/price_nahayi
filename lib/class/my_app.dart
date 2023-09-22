440bfdb2c3ec3b518ace0bc85c5b362c8b7ca764
Donya <donya.asayesh@outlook.com>
2023-09-23 00:06:45 +0330

    complateNavarForlist

--- lib/class/my_app.dart
+++ lib/class/my_app.dart
@@ -7,36 +7,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
+      supportedLocales: const [
         Locale('fa'), // farsi
       ],
       theme: ThemeData(fontFamily: 'iransans'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(elevation: 0,backgroundColor: Colors.white,
-          actions: [
+        appBar: AppBar(elevation: 0, backgroundColor: Colors.white, actions: const [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: const Icon(CupertinoIcons.money_dollar_circle,color: Colors.black,),
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              CupertinoIcons.money_dollar_circle,
+              color: Colors.black,
+            ),
           ),
           Align(
               child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 98, 0),
-            child: Text("قیمت بروز ارز",style:TextStyle(color: Colors.black) ,),
+            padding: EdgeInsets.fromLTRB(0, 0, 98, 0),
+            child: Text(
+              "قیمت بروز ارز",
+              style: TextStyle(color: Colors.black),
+            ),
           )),
           Expanded(
               child: Align(
                   alignment: Alignment.centerLeft,
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Icon(CupertinoIcons.home,color: Colors.black,),
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.home,
+                      color: Colors.black,
+                    ),
                   )))
         ]),
+        body:  Column(children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(3, 15, 8, 8),
+                child: Icon(CupertinoIcons.money_dollar),
+              ),
+              Align(child: Padding(
+                padding: EdgeInsets.fromLTRB(8, 15, 0, 8),
+                child: Text("نرخ آزاد ارز چیست ؟"),
+              ))
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: Text("نرخ ارز ها در معاملات نقدی و رایج روزانه است ،معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ،ارز و ریال را باهم تبادل می نمایند."),
+          ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(width: double.infinity,height: 30, decoration: BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.circular(50)), child: Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly,children: [Text("نام ارز"),Text("قیمت"),Text("تغییر")]),),
+        )]),
       ),
     );
   }
