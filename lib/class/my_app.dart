7fac9a3750b7d48cb28cbb321d501e8dfa1f2231
Donya <donya.asayesh@outlook.com>
2023-09-23 00:49:35 +0330

    complateListViewSeparated

--- lib/class/my_app.dart
+++ lib/class/my_app.dart
@@ -18,7 +18,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'iransans'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(elevation: 0, backgroundColor: Colors.white, actions: const [
+        backgroundColor: Colors.white,
+        appBar:
+            AppBar(elevation: 0, backgroundColor: Colors.white, actions: const [
           Padding(
             padding: EdgeInsets.all(8.0),
             child: Icon(
@@ -45,14 +47,15 @@ class MyApp extends StatelessWidget {
                     ),
                   )))
         ]),
-        body:  Column(children: [
+        body: Column(children: [
           Row(
             children: [
               Padding(
                 padding: EdgeInsets.fromLTRB(3, 15, 8, 8),
                 child: Icon(CupertinoIcons.money_dollar),
               ),
-              Align(child: Padding(
+              Align(
+                  child: Padding(
                 padding: EdgeInsets.fromLTRB(8, 15, 0, 8),
                 child: Text("نرخ آزاد ارز چیست ؟"),
               ))
@@ -60,12 +63,83 @@ class MyApp extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-            child: Text("نرخ ارز ها در معاملات نقدی و رایج روزانه است ،معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ،ارز و ریال را باهم تبادل می نمایند."),
+            child: Text(
+                "نرخ ارز ها در معاملات نقدی و رایج روزانه است ،معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ،ارز و ریال را باهم تبادل می نمایند."),
           ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(width: double.infinity,height: 30, decoration: BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.circular(50)), child: Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly,children: [Text("نام ارز"),Text("قیمت"),Text("تغییر")]),),
-        )]),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 30,
+              decoration: BoxDecoration(
+                  color: const Color.fromRGBO(158, 158, 158, 1), borderRadius: BorderRadius.circular(50)),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "نام ارز",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "قیمت",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "تغییر",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ]),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 400,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 252, 243, 217),
+                  borderRadius: BorderRadius.circular(20)),
+              child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                itemCount: 30,
+                itemBuilder: (BuildContext context, int positions) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                    child: Container(
+                      width: double.infinity,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text("دلار آمریکا"),
+                            Text("28000"),
+                            Text("-3")
+                          ]),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                    child: Container(
+                      width: double.infinity,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 255, 190, 99),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [Text(" تبلیغات")]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          )
+        ]),
       ),
     );
   }
